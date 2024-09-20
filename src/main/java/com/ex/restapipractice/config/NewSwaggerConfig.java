f5554a6c0f64795402d3500cd1c94376c6337acc
csw <susu199037@gmail.com>
2024-09-21 02:22:47 +0900

    feat: actuator

--- src/main/java/com/ex/restapipractice/config/NewSwaggerConfig.java
+++ src/main/java/com/ex/restapipractice/config/NewSwaggerConfig.java
@@ -3,23 +3,57 @@ package com.ex.restapipractice.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @OpenAPIDefinition(
-    info = @Info(title = "My Restful Service API 명세서",
-    description = "Spring Boot Rest API 명세서입니다.",
-    version = "v1.0.0")
-)
+    info = @Info(title = "My Restful Service API 명세서", description = "Spring Boot Rest API 명세서입니다.",
+        version = "v1.0.0"))
 @Configuration
 public class NewSwaggerConfig {
   @Bean
   public GroupedOpenApi customTestOpenApi() {
     String[] paths = {"/users/**", "/admin/**"};
 
-    return GroupedOpenApi.builder()
-        .group("일반 사용자와 관리자를 위한 User 도메인에 대한 API")
-        .pathsToMatch(paths)
+    return GroupedOpenApi.builder().group("일반 사용자와 관리자를 위한 User 도메인에 대한 API").pathsToMatch(paths)
         .build();
+  }
+
+  @Bean
+  public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+      ServletEndpointsSupplier servletEndpointsSupplier,
+      ControllerEndpointsSupplier controllerEndpointsSupplier,
+      EndpointMediaTypes endpointMediaTypes,
+      CorsEndpointProperties corsProperties,
+      WebEndpointProperties webEndpointProperties,
+      Environment environment) {
+    List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+    Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+    allEndpoints.addAll(webEndpoints);
+    allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+    allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+    String basePath = webEndpointProperties.getBasePath();
+    EndpointMapping endpointMapping = new EndpointMapping(basePath);
+    boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping);
+  }
+
+
+  private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
   }
 }
